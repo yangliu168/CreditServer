bc6fef8d874427e0921d663caa68bc0c01e1d60a
yangliu <willow918@163.com>
2022-09-19 11:52:06 +0800

    test.py

--- Credit_Score_Test/Credit_Score_Test/test.py
+++
@@ -1,8 +0,0 @@
-import os
-print("--------------------------------")
-print((os.path.abspath(__file__)))
-print("--------------------------------")
-print((os.path.dirname(os.path.abspath(__file__))))
-print("--------------------------------")
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
